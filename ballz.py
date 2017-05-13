db13b2fc4d432444af367ff619059caf7489c24a
David Juckes <david.a.juckes@tesco.net>
2017-05-13 21:03:55 +0100

    Added tile render

---
+++ ballz.py
@@ -0,0 +1,45 @@
+global root, tiles
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.title('Ballz')
+
+class tile:
+    def __init__(self, canvas, num, x, y):
+        self.canvas = canvas
+        self.num = num
+        self.colour = 'green'
+        self.x = x
+        self.y = y
+        self.obj = self.canvas.create_rectangle(x, y, x + self.width, y + self.height, fill=self.colour, outline=self.colour)
+        self.text = self.canvas.create_text(x + (self.width / 2), y + (self.height / 2), text=str(self.num), fill='white', font=('', 12))
+    def refresh(self):
+        self.canvas.coords(self.obj, self.x, self.y, self.x + self.width, self.y + self.height)
+        self.canvas.coords(self.text, self.x + (self.width / 2), self.y + (self.height / 2))
+    height = 32
+    width = height
+
+def new_row():
+    for row in tiles:
+        for t in row:
+            t.y += 37
+            t.refresh()
+    tiles.insert(0, [])
+    for i in range(5, 299, 37):
+        if not random.randint(1, 9) in [1, 2]:
+            tiles[0].append(tile(canvas, 100, i, 5))
+
+canvas = tk.Canvas(root, height=500, width=299, bg='black')
+
+#####
+
+tiles = []
+for i in range(4):
+    new_row()
+
+#####
+
+canvas.pack()
+
+root.mainloop()
