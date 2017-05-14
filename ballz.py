3d86366b5d3ea2ec58805072ac4fbe7bc2d4bc30
David Juckes <david.a.juckes@tesco.net>
2017-05-14 15:01:34 +0100

    Added balls which bounce off edges

--- ballz.py
+++ ballz.py
@@ -1,6 +1,6 @@
 global root, tiles, gamedata
 import tkinter as tk
-import random
+import random, threading
 
 root = tk.Tk()
 root.title('Ballz')
@@ -21,6 +21,18 @@ class tile:
     height = 32
     width = height
 
+class ball:
+    def __init__(self, canvas, x, y, dx, dy):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.obj = self.canvas.create_oval(self.x - self.radius, self.y - self.radius, self.x + self.radius, self.y + self.radius, outline='white', fill='white')
+    def refresh(self):
+        self.canvas.coords(self.obj, self.x - self.radius, self.y - self.radius, self.x + self.radius, self.y + self.radius)
+    radius = 8
+
 def new_row():
     global gamedata
     gamedata.stage += 1
@@ -37,16 +49,42 @@ def new_row():
                 inc = 0
             tiles[0].append(tile(canvas, gamedata.stage + inc, i, 5))
 
+def ball_move(canvas, balls):
+    import time
+    while True:
+        for b in balls:
+            b.x += b.dx
+            b.y += b.dy
+            b.refresh()
+            clipx = b.x + b.radius > gamedata.width or b.x - b.radius < 0
+            clipy = b.y + b.radius > gamedata.height or b.y - b.radius < 0
+            if clipx or clipy:
+                b.x -= b.dx
+                b.y -= b.dy
+            if clipx:
+                b.dx = 0 - b.dx
+            if clipy:
+                b.dy = 0 - b.dy
+        time.sleep(0.01)
+
 class gamedata:
     stage = 0
+    height = 500
+    width = 299
 
-canvas = tk.Canvas(root, height=500, width=299, bg='black')
+canvas = tk.Canvas(root, height=gamedata.height, width=gamedata.width, bg='black')
 
 #####
 
 tiles = []
 for i in range(4):
     new_row()
+
+balls = []
+for i in range(1):
+    balls.append(ball(canvas, 100, 100, -2, -2))
+
+threading.Thread(target=ball_move, args=(canvas, balls), name='Ball movement').start()
 
 #####
 
